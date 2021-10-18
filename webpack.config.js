06cff4f2ad5fb2b493b850a34ded91fedf6ff994
marcusformiga <kiko.antares@gmail.com>
2021-10-18 13:43:21 -0300

    conclusao do desafio

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     hot: true,
+    port: 3001
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),  
